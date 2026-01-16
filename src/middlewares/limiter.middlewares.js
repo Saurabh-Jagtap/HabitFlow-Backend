2d5ca73e4b21d7429756d53796b1ea45abc9a84e
Shruti J <Jagtapshruti46@gmail.com>
2026-01-16 14:02:06 +0530

    fix: update emailLimiter to improve rate limiting logic and response messages

--- src/middlewares/limiter.middlewares.js
+++ src/middlewares/limiter.middlewares.js
@@ -2,14 +2,28 @@ import rateLimit from 'express-rate-limit';
 
 // Created a limiter for Password Reset emails
 export const emailLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour window
-  max: 3, // Limit each IP to 3 requests per windowMs
-  message: {
-    success: false,
-    message: "Too many password reset emails sent. Please try again after an hour."
-  },
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 3, // Limit to 3 requests per IP
+    standardHeaders: true,
+    legacyHeaders: false,
+    
+    keyGenerator: (req, res) => {
+
+        if (req.headers["x-forwarded-for"]) {
+            return req.headers["x-forwarded-for"].split(",")[0].trim();
+        }
+        // Fallback to internal IP if header is missing 
+        return req.ip;
+    },
+    
+    handler: (req, res) => {
+        console.log(`Rate Limit Blocked: ${req.headers["x-forwarded-for"] || req.ip}`);
+        
+        res.status(429).json({
+            status: "fail",
+            message: "Too many password reset attempts. Please verify your email or try again in 15 minutes."
+        });
+    }
 });
 
 // Created a limiter for Login attempts
