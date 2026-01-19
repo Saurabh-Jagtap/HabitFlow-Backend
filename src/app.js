74e7ac157c329955958744004161b884f996a699
Shruti J <Jagtapshruti46@gmail.com>
2026-01-19 13:23:18 +0530

    added global error handling middleware

--- src/app.js
+++ src/app.js
@@ -39,4 +39,20 @@ app.use('/api/v1/habits', habitRouter)
 app.use('/api/v1', habitLogRouter)
 app.use('/api/v1', analyticsRouter)
 
+app.use((err, req, res, next) => {
+ 
+    const statusCode = err.statusCode || 500; 
+    
+    const message = err.message || "Internal Server Error";
+    
+    const errors = err.errors || [];
+
+    res.status(statusCode).json({
+        success: false,
+        message: message, 
+        errors: errors,
+        stack: process.env.NODE_ENV === "development" ? err.stack : undefined
+    });
+});
+
 export default app
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,15 +1,23 @@
-class ApiError extends Error{
-    constructor (
-        status,
+class ApiError extends Error {
+    constructor(
+        statusCode, 
         message = "Something went wrong",
-        error = []
-    ){
+        errors = [], 
+        stack = ""
+    ) {
         super(message)
+        this.statusCode = statusCode 
         this.data = null
-        this.status = status
-        this.error = error
+        this.message = message
         this.success = false
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 }
 
-export {ApiError}
+export { ApiError }
